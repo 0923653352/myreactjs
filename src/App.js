3ab4b7b183ecae4d2b885f3711bd979a6269140a
Nawathpolkaw <nawathpolkaw@gmail.com>
2021-09-28 17:22:20 +0700

    react.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import kaw from "./kaw.png"; //ใส่รูปอื่น
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={kaw} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
