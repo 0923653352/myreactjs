6f5b7cda45238b5cdabd0767296d754a44ad1fd3
Nawathpolkaw <nawathpolkaw@gmail.com>
2021-10-05 16:39:58 +0700

    done

--- src/components/Theme.js
+++ src/components/Theme.js
@@ -530,7 +530,8 @@ export default function Theme(props) {
               <div className="container-fluid">
                 {/* Page Heading */}
                 <div className="d-sm-flex align-items-center justify-content-between mb-4">
-                  <h1 className="h3 mb-0 text-gray-800">Dashboard</h1>
+                  <h1 className="h3 mb-0 text-gray-800">{props.title}</h1>
+                  {/* //ตาม  {props.Title} Ant cat brid ได้ */}
                   <a
                     href="#"
                     className="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"
